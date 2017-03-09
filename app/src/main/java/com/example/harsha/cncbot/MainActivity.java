70ddc367bb3969f85037fc8ceb33f8881da65927
Harsha Bandi <harsha44467@gmail.com>
2017-03-09 21:29:59 +0530

    Added button communication Addings lines to float array refer android dev guide

--- app/src/main/java/com/example/harsha/cncbot/MainActivity.java
+++ app/src/main/java/com/example/harsha/cncbot/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.harsha.cncbot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button square,triangle,clr, line;
     @Override
@@ -17,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
         triangle=(Button)findViewById(R.id.btn_triangle);
         line=(Button)findViewById(R.id.btn_line);
         clr=(Button)findViewById(R.id.btn_clear);
-        square.setOnClickListener();
+        square.setOnClickListener(this);
+        triangle.setOnClickListener(this);
+        line.setOnClickListener(this);
+        clr.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.btn_square:
+                touchControl.btn=2;
+                break;
+            case R.id.btn_triangle:
+                touchControl.btn=3;
+                break;
+            case R.id.btn_line:
+                touchControl.btn=1;
+                break;
+            case R.id.btn_clear:
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/harsha/cncbot/touchControl.java
+++ app/src/main/java/com/example/harsha/cncbot/touchControl.java
@@ -8,6 +8,7 @@ import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Harsha on 3/6/2017.
@@ -16,7 +17,10 @@ import android.view.View;
 public class touchControl extends View {
     private Paint paint = new Paint();
     private Path path = new Path();
-    public int btn;
+    public static int btn;
+    public float oxpos,oypos,nxpos,nypos;
+    public int flag,l;
+    public float[] lines = new float[40];
 
     public touchControl(Context context, AttributeSet attributes) {
         super(context, attributes);
@@ -25,32 +29,53 @@ public class touchControl extends View {
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(5f);
+        btn=1;
+        flag=0;
+        l=0;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawPath(path, paint);
+        if(btn==1)
+            canvas.drawPath(path, paint);
+        else
+            canvas.drawLines(lines, paint);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float xpos = event.getX();
-        float ypos = event.getY();
+        if(flag==0) {
+            reset(event);
+            flag=1;
+        }
+        nxpos=event.getX();
+        nypos=event.getY();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                path.moveTo(xpos, ypos);
+                path.moveTo(nxpos, nypos);
                 return true;
             case MotionEvent.ACTION_MOVE:
-                path.lineTo(xpos, ypos);
+                path.lineTo(nxpos, nypos);
                 break;
             case MotionEvent.ACTION_UP:
+                flag=0;
                 break;
             default:
                 return false;
         }
+        if(btn==2){
+            lines[l++]=oxpos;
+            lines[l++]=oypos;
+            lines[l++]=nxpos;
+            lines[l++]=nypos;
+        }
         invalidate();
         return true;
+    }
+    public void reset(MotionEvent event){
+        oxpos=event.getX();
+        oypos=event.getY();
     }
 }
 
